3b6bb71478ec7d5376ffc47003ab5c96e54749fd
Antonio Bibiano <antbbn@gmail.com>
2016-10-07 18:34:30 +1100

    Added beautifulsoup parsing of request content to obtain first working example

--- get_price_requests.py
+++ get_price_requests.py
@@ -1,6 +1,7 @@
 import sys
 import requests
 import json
+from bs4 import BeautifulSoup
 
 url_all_countries = "http://international.o2.co.uk/internationaltariffs/getallcountries"
 url_country = "http://international.o2.co.uk/internationaltariffs/getintlcallcosts?countryId={}"
@@ -34,7 +35,17 @@ for country_name, country_id in countries_ids_to_get.iteritems():
   if country_req is None or country_req.status_code != requests.codes.ok:
     print "Failed request"
     sys.exit(-1)
-  with open("{}.html".format(country_name),"w") as testfile:
-    testfile.write(country_req.content)
+  #with open("{}.html".format(country_name),"w") as testfile:
+  #  testfile.write(country_req.content)
+  soup = BeautifulSoup(country_req.content)
+  print country_name,
+  for div_id in ["paymonthlyTariffPlan","payandgoTariffPlan"]:
+    div = soup.find(id=div_id)
+    standard_rates_table = div.find(id='standardRatesTable')
+    trs = standard_rates_table.find_all('tr')
+    tds = trs[0].find_all('td')
+    print div_id, float(tds[1].string[1:]),
+  print
+
 
 
