85d1aed05576c95f745293be739622b385c712fc
Antonio Bibiano <antbbn@gmail.com>
2016-10-07 19:46:36 +1100

    Refactore selenium version with objects

--- get_price_selenium.py
+++ get_price_selenium.py
@@ -1,30 +1,50 @@
-import sys
-import requests
-import json
-from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 import time
 
-url = "http://international.o2.co.uk/internationaltariffs/calling_abroad_from_uk"
+class O2GetPrice:
+  url = "http://international.o2.co.uk/internationaltariffs/calling_abroad_from_uk"
+  def __init__(self, url=None):
+    if url is not None:
+      self.url = url
 
-countries_to_get = ["Canada", "Germany", "Iceland", "Pakistan", "Singapore", "South Africa"]
+  driver_path = "./selenium_drivers/chromedriver"
+  def start_browser(self, path=None):
+    if path is not None:
+      self.driver_path = path
+    self.browser = webdriver.Chrome(executable_path = self.driver_path)
+    self.browser.get(self.url)
 
-#path_to_phantomjs = './phantomjs-2.1.1-macosx/bin/phantomjs' # change path as needed
-#browser = webdriver.PhantomJS(executable_path = path_to_phantomjs)
-path_to_chromedriver = './selenium_drivers/chromedriver'
-browser = webdriver.Chrome(executable_path = path_to_chromedriver)
+  def stop_browser(self):
+    self.browser.quit()
 
-browser.get(url)
-for country in countries_to_get:
-  inputarea = browser.find_element_by_id('countryName')  
-  inputarea.clear()
-  inputarea.send_keys(country+'\n')
-  time.sleep(1)
-  print country,
-  for div_id in ["paymonthlyTariffPlan","payandgoTariffPlan"]:
-    #div = browser.find_element_by_id(div_id)
-    price = browser.find_element_by_xpath('//*[@id="{}"]//*[@id="standardRatesTable"]/tbody/tr[1]/td[2]'.format(div_id))
-    print div_id, price.get_attribute('innerText')
-  time.sleep(3)
-browser.quit()
+  # Necessary methods to use in a with statement
+  def __enter__(self):
+    self.start_browser()
+    return self
+
+  def __exit__(self, exc_type, exc_value, traceback):
+    self.stop_browser
+
+  # accepts a country string and returns an array with two prices
+  # the first one is the "Pay Monthly", the second one is the "Pay & Go"
+  def get_standard_prices(self,country):
+    inputarea = self.browser.find_element_by_id('countryName')  
+    inputarea.clear()
+    inputarea.send_keys(country+'\n')
+    time.sleep(1)
+    result = []
+    for div_id in ["paymonthlyTariffPlan","payandgoTariffPlan"]:
+      price = self.browser.find_element_by_xpath('//*[@id="{}"]//*[@id="standardRatesTable"]/tbody/tr[1]/td[2]'.format(div_id))
+      price_str = price.get_attribute('innerText')
+      result.append(float(price_str[1:])) # the [1:] gets rid of the pound symbol
+    time.sleep(3)
+    return result
+
+
+
+if __name__ == "__main__":
+  countries_to_get = ["Canada", "Germany", "Iceland", "Pakistan", "Singapore", "South Africa"]
+
+  with O2GetPrice() as o2:
+    for country in countries_to_get:
+      print country, o2.get_standard_prices(country)
